589bbf0d1be1ba108b9cc019be618f6dee8b8bd5
PrathameshHajare <97085521+PrathameshHajare@users.noreply.github.com>
2022-01-04 13:00:08 +0530

    Add files via upload

---
+++ CustomerDetails.java
@@ -0,0 +1,16 @@
+/*
+#Task 2
+Registered ID= prtahameshhajare10@gmail.com
+Prathamesh Hanumantrao Hajare
+*/
+class CustomerDetails{
+  public static void main(String[] args)
+ {
+  int ID= 101;
+  String Name= "Jones";
+  char Gender= 'M';
+  String Email= "customer1@gmail.com";
+  long mobile= 1122334455L;
+  System.out.println("The customer deatils are: /n%d /n%c /n%c /n%s /n%d",ID,Name,Gender,Email,mobile);
+ }
+}
